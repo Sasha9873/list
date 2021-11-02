a37f328bae260617e28a836d2e81971aad62beb0
Sasha9873 <78866386+Sasha9873@users.noreply.github.com>
2021-11-02 22:24:38 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,224 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+//что делать, если вставляем после эл, кот еще нет в списке, как чистить память?
+
+int CHANGE = 2;
+
+typedef enum errors{
+    ALL_OK    =  0,
+    NO_MEMORY = -1,
+}errors_t;
+
+struct List{
+    int* data;
+    int* next;
+    int capacity = 2;
+    int head = 0;
+    int tail = 0;
+};
+
+int list_ctor(List* Lst);
+int list_add(List* Lst, int place, int value);
+int list_delete(List* Lst, int place);
+
+int main()
+{
+    List Lst = {};
+    list_ctor(&Lst);
+
+    list_add(&Lst, 4, 9);
+    printf("head = %d tail = %d\n", Lst.head, Lst.tail);
+    int i = 0;
+    for(i = 0; i < Lst.capacity; i++)
+            printf("%3d ", i);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.data[i]);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.next[i]);
+
+
+    list_add(&Lst, 1, 7);
+    printf("head = %d tail = %d\n", Lst.head, Lst.tail);
+    for(i = 0; i < Lst.capacity; i++)
+            printf("%3d ", i);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.data[i]);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.next[i]);
+
+
+    list_add(&Lst, 1, 6);
+    printf("head = %d tail = %d\n", Lst.head, Lst.tail);
+    for(i = 0; i < Lst.capacity; i++)
+            printf("%3d ", i);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.data[i]);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.next[i]);
+
+
+    list_add(&Lst, 3, 44);
+    printf("head = %d tail = %d\n", Lst.head, Lst.tail);
+
+    for(i = 0; i < Lst.capacity; i++)
+            printf("%3d ", i);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.data[i]);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.next[i]);
+
+    list_delete(&Lst, 2);
+    printf("head = %d tail = %d\n", Lst.head, Lst.tail);
+
+    for(i = 0; i < Lst.capacity; i++)
+            printf("%3d ", i);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.data[i]);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.next[i]);
+
+
+    list_add(&Lst, 4, 37);
+    printf("head = %d tail = %d\n", Lst.head, Lst.tail);
+
+    for(i = 0; i < Lst.capacity; i++)
+            printf("%3d ", i);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.data[i]);
+
+    printf("\n");
+
+    for(i = 0; i < Lst.capacity; i++)
+        printf("%3d ", Lst.next[i]);
+
+    return 0;
+}
+
+int list_ctor(List* Lst)
+{
+    Lst->data = (int*)calloc(Lst->capacity, sizeof(int));
+    Lst->next = (int*)calloc(Lst->capacity, sizeof(int));
+    memset(Lst->next, -1, Lst->capacity*sizeof(int));
+
+    if(Lst->capacity > 0){
+        (Lst->data)[0] = 0;
+        (Lst->next)[0] = 0;
+    }/**/
+
+    return 0;
+}
+
+int list_add(List* Lst, int place, int value)
+{
+    int num = Lst->capacity - 1, cur_num = 1;
+    printf("\ncapacity = %d\nnum: ", Lst->capacity);
+    for(cur_num = 1; cur_num < Lst->capacity; cur_num++){
+        printf("%d ", cur_num);
+        if(Lst->next[cur_num] == -1){
+            num = cur_num;
+            break;
+        }
+
+    }
+    printf("\nnum = %d next = %d\n", num, Lst->next[num]);
+
+    int* new_data_memory = NULL;
+    int* new_next_memory = NULL;
+
+    if(num == Lst->capacity - 1 && Lst->next[num] != -1){
+        new_data_memory = (int*)realloc(Lst->data, sizeof(int)*CHANGE*Lst->capacity);
+        new_next_memory = (int*)realloc(Lst->next, sizeof(int)*CHANGE*Lst->capacity);
+
+        if(new_data_memory == NULL || new_next_memory == NULL)
+            return NO_MEMORY;
+        else{
+            Lst->capacity *= CHANGE;
+
+            Lst->data = new_data_memory;
+            Lst->next = new_next_memory;
+
+            memset(Lst->data + num + 1, 0, (Lst->capacity - num - 1)*sizeof(int));
+            memset(Lst->next + num + 1, -1, (Lst->capacity - num - 1)*sizeof(int));
+
+            num++;
+        }
+    }
+
+    Lst->data[num] = value;
+
+    if(Lst->head == 0){
+        Lst->next[num] = 0;
+        Lst->head = num;
+        Lst->tail = num;
+    }
+    else{
+        Lst->next[num] = Lst->next[place];
+        Lst->next[place] = num;
+
+        if(Lst->next[num] == 0)
+            Lst->tail = num;
+    }
+
+    return num;
+}
+
+int list_delete(List* Lst, int place)
+{
+    int num = 1, cur_num;
+    printf("\ncapacity = %d\nnum: ", Lst->capacity);
+    for(cur_num = 1; cur_num < Lst->capacity; cur_num++){
+        printf("%d ", cur_num);
+        if(Lst->next[cur_num] == place){
+            num = cur_num;
+            break;
+        }
+    }
+
+    printf("\nnum = %d\n", num);
+
+    Lst->next[num] = Lst->next[place];
+
+    if(Lst->head == place)
+        Lst->head = num;
+
+    if(Lst->tail == place)
+        Lst->tail = num;
+
+    Lst->data[place] = 0;
+    Lst->next[place] = -1;
+}
